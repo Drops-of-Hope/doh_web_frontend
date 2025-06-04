eb54455e77023a6674babdc34f70e61e6d914614
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-06-04 13:22:17 +0530

    feat: structured pages and set up index.ts files for cleaner component exports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import HomePage from '@/pages/HomePage';
+import NavBar from "@/components/organisms/NavBar";
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <main>
+      <NavBar />
+      
+    </main>
+  );
 }
+
+// root page (eg- landing page)
+// All pages must be created within the app folder 
+
+//structure
+// eg- login page must be inside app->auth->login->page.tsx
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+// components can be exported through this file altogether
+//will be implemented soon
+
