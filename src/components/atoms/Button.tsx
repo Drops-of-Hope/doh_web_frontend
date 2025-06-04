e6306170fa10ae082ea646e9980d0e47ed3d2251
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-06-04 12:49:14 +0530

    chore: setup atomic design folder structure

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,12 @@
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+  return (
+    <button onClick={onClick} className="">
+      {children}
+    </button>
+  );
+};
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/components/organisms/Navbar";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <NavBar />
+      HomePage
+    </div>
+  );
+}
