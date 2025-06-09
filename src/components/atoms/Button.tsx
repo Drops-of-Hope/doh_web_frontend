98a1cf92c746f8727ad0a657dd65e145d5ac471a
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-06-09 14:29:14 +0530

    fix: remove unused Image import to pass lint

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from 'react'
-import Image from 'next/image'
 import { ButtonProps } from '../../../types';
 
 const Button = ({title, containerStyles, handleClick} : ButtonProps) => {
