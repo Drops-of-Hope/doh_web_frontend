8db79f420d012d87533302bfcddd675177be0fac
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-06-20 18:52:57 +0530

    fix: navbar width fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <main>
       <NavBar />
       <Hero />
-      <Hero />
     </main>
   );
 }
--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
           : 'bg-transparent'
       }`}
     >
-      <div className="max-w-7xl mx-auto flex items-center justify-between">
+      <div className="mx-auto flex items-center justify-between">
         
         <div className="flex items-center -space-x-2">
           <Image
