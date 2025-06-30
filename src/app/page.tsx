d7d23ebfb6ad54910758aa6b58652189a9130cff
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-06-30 11:37:24 +0530

    fix: resolved lint issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-import { Hero, NavBar, Footer } from "@/components";
+import { Hero, NavBar } from "@/components";
 
 export default function Home() {
 
   return (
     <main>
-      {/* <NavBar />
+      <NavBar />
       <Hero />
       <div id="section-1" className="h-[100vh] flex flex-col items-center justify-center">
         next section to test the hero button scrollable function
-      </div> */}
+      </div>
 
-      <Footer />
     </main>
   );
 }
