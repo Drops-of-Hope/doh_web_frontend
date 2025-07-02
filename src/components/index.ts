484bd60fc4457a8498467958087bebf2638ab3ef
Thirani Imanya <thiraniwork@gmail.com>
2025-07-03 00:59:27 +0530

    feat:metric card implementation

--- src/components/index.ts
+++ src/components/index.ts
@@ -19,6 +19,7 @@ import DonationBanner from "./organisms/DonationBanner";
 import ProfileHeader from "./organisms/ProfileHeader";
 import LoginActivity from "./organisms/LoginActivity";
 import AuditLogs from "./organisms/AuditLogs";
+import MetricCard from "./molecules/MetricCard";
 
 export {
     Button,
@@ -41,6 +42,7 @@ export {
     DonationBanner,
     ProfileHeader,
     LoginActivity,
-    AuditLogs
+    AuditLogs,
+    MetricCard
 }
 
