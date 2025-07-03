2e2fd614957ae44c6c39c95b131be7df1b31cdc7
imnot-naveen <naveenharinda2@gmail.com>
2025-07-03 10:51:45 +0530

    fix: resolved lint issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
-import { NavBar, Hero, MetricCard } from "@/components";
+import { NavBar, Hero } from "@/components";
 
 export default async function Home() {
   
   return (  
     <main>
-      {/* <NavBar />
+      <NavBar />
       <Hero />
       <div id="section-1" className="h-[100vh] flex flex-col items-center justify-center">
         next section to test the hero button scrollable function
-      </div> */}
-
-      <MetricCard
-        icon="./assets/BloodDrop.png" 
-        iconBgColor="#FB7373"
-        heading={"Total \nDonations"}
-        body="Total number of donations made"
-        count={1200}
-      />
-
+      </div>
     </main>
   );
 }
