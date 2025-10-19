45860aceccdc30e84b353ac39b7a166fbd041139
Tharindu DM <tharindudm@hotmail.com>
2025-10-19 14:48:15 +0530

    fix: set output option for static HTML file generation in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // This tells Next.js to create static HTML files
+  //
 };
 
 export default nextConfig;
